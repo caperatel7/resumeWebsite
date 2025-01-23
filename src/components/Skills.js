75b9cbb3e53fea20e1a8daa852a24624a0303317
Kaija Collette <kaijam7collette@gmail.com>
2025-01-22 19:34:19 -0600

    Resume Version 1.0.0

---
+++ src/components/Skills.js
@@ -0,0 +1,40 @@
+// import meter3 from "../assets/img/meter3.svg";
+import 'react-multi-carousel/lib/styles.css';
+// import arrow1 from "../assets/img/arrow1.svg";
+// import arrow2 from "../assets/img/arrow2.svg";
+// import colorSharp from "../assets/img/color-sharp.png"
+// import html from "../assets/img/html.png";
+// import css from "../assets/img/css.png";
+// import javascript from "../assets/img/javascript.png";
+// import sass from "../assets/img/sass.png";
+// import react from "../assets/img/react.png";
+// import nextjs from "../assets/img/nextjs.png";
+// import azure from "../assets/img/azure.png";
+// import vscode from "../assets/img/vscode.png";
+// import python from "../assets/img/python.png";
+
+export const Skills = () => {
+    return (
+      <section className="skill" id="skills">
+        <div className="logo container">
+          <div className="row">
+            <div className="col-12">
+              <div className="marquee">
+                <div className="track">
+                {/* <img src={html} alt="HTML" width="128" />
+                <img src={css} alt="CSS" width="128" />
+                <img src={javascript} alt="JavaScript" width="20" />
+                <img src={sass} alt="Sass" width="128" />
+                <img src={react} alt="React" width="128" />
+                <img src={nextjs} alt="Next.js" width="128" />
+                <img src={azure} alt="Azure" width="128" />
+                <img src={vscode} alt="VS Code" width="128" />
+                <img src={python} alt="Python" width="128" /> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  };
